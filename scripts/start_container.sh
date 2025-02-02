9c345dc7cc90e9413fd169b280835e957ebe6536
Dharmendra Bhagat <bhagatdharmendra0@gmail.com>
2025-02-02 12:20:14 +0530

    Added

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull bhagatdharmendra0/simple-python-flask-app:v1.0.3
-
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 941377157113.dkr.ecr.ap-south-1.amazonaws.com
+docker pull 941377157113.dkr.ecr.ap-south-1.amazonaws.com/workspace:v1.0.3
 # Run the Docker image as a container
-docker run -d -p 5001:5000 bhagatdharmendra0/simple-python-flask-app:v1.0.3
+docker run -d -p 5001:5000 941377157113.dkr.ecr.ap-south-1.amazonaws.com/workspace:v1.0.3
