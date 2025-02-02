3cd9b44f39342f3549e60027aaa374f9b92a11f2
Dharmendra Bhagat <bhagatdharmendra0@gmail.com>
2025-02-02 12:00:22 +0530

    Jano Do

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull bhagatdharmendra0/simple-python-flask-app:v1.0.3
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5001:5000 bhagatdharmendra0/simple-python-flask-app:v1.0.3
